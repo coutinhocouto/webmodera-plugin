6f3abf5ada190a8ee81bfd4a0eb27bd82da58a06
coutinhocouto <coutinhocouto@hotmail.com>
2021-06-25 12:58:40 -0300

    1.0.6

--- global-videos/global-videos.php
+++ global-videos/global-videos.php
@@ -9,6 +9,19 @@ Author URI: https://www.globalvideos.com.br
 License: GPL2
  */
 
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/coutinhocouto/webmodera-plugin',
+	__FILE__,
+	'global-videos'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+//Optional: If you're using a private repository, specify the access token like this:
+$myUpdateChecker->setAuthentication('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');
+
 add_role('medicos', 'Médicos', array('read' => true));
 add_role('nao_medicos', 'Não Médicos', array('read' => true));
 add_role('staff', 'Staff', array('read' => true));
@@ -25,5 +38,4 @@ include( plugin_dir_path( __FILE__ ) . 'painel_admin.php');
 include( plugin_dir_path( __FILE__ ) . 'restict_pages.php');
 include( plugin_dir_path( __FILE__ ) . 'woocommerce.php');
 include( plugin_dir_path( __FILE__ ) . 'custom_functions.php');
-include( plugin_dir_path( __FILE__ ) . 'update.php');
 
