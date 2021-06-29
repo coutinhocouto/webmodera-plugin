5525b90ee294721e2b8cd2510b05a2cade3daed7
coutinhocouto <coutinhocouto@hotmail.com>
2021-06-29 17:57:09 -0300

    1.1.1

--- acesso_gravado.php
+++ acesso_gravado.php
@@ -1,33 +1,25 @@
 <?php
 
-function global_acesso_gravado($atts)
+function global_acessos()
 {
 
-    $atts = shortcode_atts(
-        array(
-            'live' => '',
-        ),
-        $atts,
-        'global_cadastra_form'
-    );
-
     $current_user = wp_get_current_user();
+    global $wp;
     $email = $current_user->user_email;
-    $live = $atts['live'];
+    $evento = get_option('evento_global');
+    $link = home_url( $wp->request );
 
     ?>
 
 <script>
     jQuery(document).ready(function( $ ){
-
-        var email = $("#email").val();
-
-        var dataString='{"live": <?php echo $live; ?>, "email":"<?php echo $email; ?>","tipo": 2}';
+		
+        var dataString='{"evento": <?php echo $evento; ?>, "email":"<?php echo $email; ?>" , "link":"<?php echo $link; ?>","tipo": 1}';
 
         console.log(dataString);
 
         $.post({
-            url:"https://4k5zxy0dui.execute-api.us-east-1.amazonaws.com/webmodera/inscrito-aovivo",
+            url:"https://4k5zxy0dui.execute-api.us-east-1.amazonaws.com/webmodera/acessos",
             data: dataString,
             dataType: 'json',
             crossDomain: true,
@@ -44,9 +36,36 @@ function global_acesso_gravado($atts)
         });
 
     });
+	
+	window.addEventListener("beforeunload", function (e) {        
+      
+		var dataString='{"evento": <?php echo $evento; ?>, "email":"<?php echo $email; ?>" , "link":"<?php echo $link; ?>","tipo": 2}';
+
+        console.log(dataString);
+
+        jQuery.post({
+            url:"https://4k5zxy0dui.execute-api.us-east-1.amazonaws.com/webmodera/acessos",
+            data: dataString,
+            dataType: 'json',
+            crossDomain: true,
+            cache: false,
+            beforeSend: function(){ console.log("enviando")},
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                console.log(XMLHttpRequest);
+                console.log(textStatus);
+                console.log(errorThrown);
+            },
+            success: function() {
+                console.log("enviou")
+            }
+        });
+		
+		
+      return;
+    });
 </script>
 
 <?php
 }
 
-add_shortcode('acesso_gravado', 'global_acesso_gravado');
+add_shortcode('acessos', 'global_acessos');
