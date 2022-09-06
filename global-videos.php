6377905184df008e51e6839af22d661b0e769bed
coutinhocouto <coutinhocouto@hotmail.com>
2022-09-06 10:54:50 -0300

    1.7.3

--- global-videos.php
+++ global-videos.php
@@ -3,7 +3,7 @@
 Plugin Name: Global Videos
 Plugin URI: https://www.globalvideos.com.br
 description: Plugins para os sites de eventos
-Version: 1.7.2
+Version: 1.7.3
 Author: Global Videos
 Author URI: https://www.globalvideos.com.br
 License: GPL2
@@ -15,52 +15,9 @@ $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
 	__FILE__,
 	'global-videos'
 );
-
-//Set the branch that contains the stable release.
 $myUpdateChecker->setBranch('main');
 
-//Optional: If you're using a private repository, specify the access token like this:
-//$myUpdateChecker->setAuthentication('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');
-
-add_role('medicos', 'Médicos', array('read' => true));
-add_role('nao_medicos', 'Não Médicos', array('read' => true));
-add_role('staff', 'Staff', array('read' => true));
-add_role('convidado', 'Convidado', array('read' => true));
-add_role('vip', 'Vip', array('read' => true));
-
-global $global_db_version;
-$global_db_version = '1.7.2';
-
-function global_install() {
-	global $wpdb;
-	global $global_db_version;
-
-	$table_name = $wpdb->prefix . 'global_codigos';
-	
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE $table_name (
-		id bigint(20) NOT NULL AUTO_INCREMENT,
-		user_id bigint(20) NOT NULL,
-		codigo DATETIME(20) DEFAULT CURRENT_TIMESTAMP NOT NULL,
-		PRIMARY KEY  (id)
-	) $charset_collate;";
-
-	require_once ABSPATH . 'wp-admin/includes/upgrade.php';
-	dbDelta( $sql );
-
-	add_option( 'global_db_version', $global_db_version );
-}
-register_activation_hook( __FILE__, 'global_install' );
-
-function myplugin_update_db_check() {
-    global $global_db_version;
-    if ( get_site_option( 'global_db_version' ) != $global_db_version ) {
-        global_install();
-    }
-}
-add_action( 'plugins_loaded', 'myplugin_update_db_check' )
-
+include( plugin_dir_path( __FILE__ ) . 'installer.php');
 include( plugin_dir_path( __FILE__ ) . 'btn_aovivo.php');
 include( plugin_dir_path( __FILE__ ) . 'acessos.php');
 include( plugin_dir_path( __FILE__ ) . 'acesso_gravado.php');
---
+++ installer.php
@@ -0,0 +1,31 @@
+<?php 
+
+add_role('medicos', 'Médicos', array('read' => true));
+add_role('nao_medicos', 'Não Médicos', array('read' => true));
+add_role('staff', 'Staff', array('read' => true));
+add_role('convidado', 'Convidado', array('read' => true));
+add_role('vip', 'Vip', array('read' => true));
+
+/* ------------------------------------------------------------ */
+
+register_activation_hook(__file__, 'installer');
+function installer(){
+    global $wpdb;
+    $table_name = $wpdb->prefix . "global_codigos";
+    $global_version = '1.7.3';
+    $charset_collate = $wpdb->get_charset_collate();
+
+    if ( $wpdb->get_var("SHOW TABLES LIKE '{$table_name}'") != $table_name ) {
+
+        $sql = "CREATE TABLE $table_name (
+            id bigint(20) NOT NULL AUTO_INCREMENT,
+            user_id bigint(20) NOT NULL,
+            codigo DATETIME(20) DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            PRIMARY KEY  (id)
+        ) $charset_collate;";
+
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+        dbDelta($sql);
+        add_option('webmodera', $global_version);
+    }
+}
