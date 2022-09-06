007aa7a765e0faa54a22a24a30842662e8710bfe
coutinhocouto <coutinhocouto@hotmail.com>
2022-09-06 11:14:21 -0300

    1.7.4

--- installer.php
+++ installer.php
@@ -8,25 +8,18 @@ add_role('vip', 'Vip', array('read' => true));
 
 /* ------------------------------------------------------------ */
 
-register_activation_hook(__file__, 'installer');
-function installer(){
+function global_table_install() {
     global $wpdb;
-    $table_name = $wpdb->prefix . "global_codigos";
-    $global_version = '1.7.4';
-    $charset_collate = $wpdb->get_charset_collate();
-
-    if ( $wpdb->get_var("SHOW TABLES LIKE '{$table_name}'") != $table_name ) {
-
-        $sql = "CREATE TABLE $table_name (
-            id bigint(20) NOT NULL AUTO_INCREMENT,
-            user_id bigint(20) NOT NULL,
-            codigo varchar(255) NOT NULL, 
-            usado DATETIME DEFAULT CURRENT_TIMESTAMP,
-            PRIMARY KEY  (id)
-        ) $charset_collate;";
-
-        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-        dbDelta($sql);
-        add_option('webmodera', $global_version);
-    }
+    global $charset_collate;
+    $table_name = $wpdb->prefix . 'global_codigos';
+    $sql = "CREATE TABLE IF NOT EXISTS $table_name (
+        id bigint(20) NOT NULL AUTO_INCREMENT,
+        user_id bigint(20) NOT NULL,
+        codigo varchar(255) NOT NULL, 
+        usado DATETIME DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY  (id)
+    )$charset_collate;";
+     require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+     dbDelta( $sql );
 }
+register_activation_hook(__FILE__,'global_table_install');
